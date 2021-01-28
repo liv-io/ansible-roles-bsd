983e054165d3ba18426acb735c857aadbf513b9c
l <l@liv.io>
2021-01-28 23:12:55 +0100

    Declutter code - phase 1,2

--- bash/templates/FreeBSD/bash.bash
+++ bash/templates/FreeBSD/bash.bash
@@ -1,11 +1,7 @@
 {{ansible_managed|comment(decoration='#')}}
 
-#-------------------------------------------------------------------------------
 # NOTE
-#-------------------------------------------------------------------------------
 #
 # All default Bash environment variables have been moved from 'bash.bash' to
 # 'zz_bash.bash'. Files in '/etc/profile.d/' are loaded in alphabetical order.
 # Changing the order helps to ensure important variables do not get overwritten.
-#
-#-------------------------------------------------------------------------------
--- shell/templates/OpenBSD/shell.sh
+++ shell/templates/OpenBSD/shell.sh
@@ -1,11 +1,7 @@
 {{ansible_managed|comment(decoration='#')}}
 
-#-------------------------------------------------------------------------------
 # NOTE
-#-------------------------------------------------------------------------------
 #
 # All default Shell environment variables have been moved from 'shell.sh' to
 # 'z_shell.sh'. Files in '/etc/profile.d/' are loaded in alphabetical order.
 # Changing the order helps to ensure important variables do not get overwritten.
-#
-#-------------------------------------------------------------------------------
